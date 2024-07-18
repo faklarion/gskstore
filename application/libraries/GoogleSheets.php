07eba2eb368bac504627f63faa4a314c2c8bef71
faklarion <faisal.hazard17@gmail.com>
2024-07-18 11:51:08 +0800

    test

--- application/libraries/GoogleSheets.php
+++ application/libraries/GoogleSheets.php
@@ -7,13 +7,26 @@ class GoogleSheets {
     protected $service;
 
     public function __construct() {
+        // URL of the credentials file
+        $url = 'https://dl.dropboxusercontent.com/scl/fi/6aj0t90rn7crhg0yeluzs/credentials.json?rlkey=y7uaboayixhcjlpqf3nfn0ctt&st=nx0qvw9f&dl=0';
+        
+        // Path to save the credentials file temporarily
+        $tempPath = FCPATH . 'application/config/temp_credentials.json';
+        
+        // Download the file
+        file_put_contents($tempPath, file_get_contents($url));
+
+        // Initialize the Google client
         $this->client = new Google_Client();
         $this->client->setApplicationName('Google Sheets API with CodeIgniter');
         $this->client->setScopes([Google_Service_Sheets::SPREADSHEETS]);
-        $this->client->setAuthConfig(APPPATH . 'config/credentials.json'); // Ganti dengan path ke credentials.json Anda
+        $this->client->setAuthConfig($tempPath); // Use the downloaded credentials file
         $this->client->setAccessType('offline');
 
         $this->service = new Google_Service_Sheets($this->client);
+
+        // Optionally, delete the temporary file after use
+        // unlink($tempPath);
     }
 
     public function readSheet($spreadsheetId, $range) {
